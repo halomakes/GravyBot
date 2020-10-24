4c8947651d610e72aa1645035af8d8dea89f3b6f
halomademeapc <alex@halomademeapc.com>
2020-10-24 00:29:45 -0400

    start orchestrator tests

--- tests/GravyBot.Commands.Tests/OrchestratorShould.cs
+++ tests/GravyBot.Commands.Tests/OrchestratorShould.cs
@@ -1,5 +1,6 @@
 ﻿using GravyIrc.Messages;
 using Microsoft.Extensions.Options;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
@@ -22,7 +23,7 @@ namespace GravyBot.Commands.Tests
         }
 
         [Fact]
-        public async Task Invoke_String_Params()
+        public async Task Accept_String_Param()
         {
             var msg = new PrivateMessage("#test", ".cb hello potato");
             var responses = await orchestrator.RespondAsync(msg).ToListAsync();
@@ -31,10 +32,85 @@ namespace GravyBot.Commands.Tests
             Assert.Equal("hello, potato", resp.Message);
         }
 
+        [Fact]
+        public async Task Accept_Multiple_Strings()
+        {
+            var msg = new PrivateMessage("#test", ".cb potato yeet yote");
+            var responses = await orchestrator.RespondAsync(msg).ToListAsync();
+            Assert.NotEmpty(responses);
+            var resp = responses.FirstOrDefault() as PrivateMessage;
+            Assert.Equal("got yeet and yote", resp.Message);
+        }
+
+        [Fact]
+        public async Task Accept_Multiple_Parameterless()
+        {
+            var msg = new PrivateMessage("#test", ".cb ping");
+            var responses = await orchestrator.RespondAsync(msg).ToListAsync();
+            Assert.NotEmpty(responses);
+            var resp = responses.FirstOrDefault() as PrivateMessage;
+            Assert.Equal("pong", resp.Message);
+        }
+
+        [Fact]
+        public async Task Convert_Standard_Types()
+        {
+            var msg = new PrivateMessage("#test", ".cb add 420 69");
+            var responses = await orchestrator.RespondAsync(msg).ToListAsync();
+            Assert.NotEmpty(responses);
+            var resp = responses.FirstOrDefault() as PrivateMessage;
+            Assert.Equal("489", resp.Message);
+        }
+
+        [Fact]
+        public async Task Convert_Dates()
+        {
+            var msg = new PrivateMessage("#test", ".cb schedule potato 12-23-2020");
+            var responses = await orchestrator.RespondAsync(msg).ToListAsync();
+            Assert.NotEmpty(responses);
+            var resp = responses.FirstOrDefault() as PrivateMessage;
+            Assert.Equal("potato at 12/23/2020", resp.Message);
+        }
+
+        [Fact]
+        public async Task Block_DirectOnly_In_Channel()
+        {
+            var msg = new PrivateMessage("#public", ".cb secret i suck eggs");
+            var responses = await orchestrator.RespondAsync(msg).ToListAsync();
+            Assert.NotEmpty(responses);
+            Assert.IsAssignableFrom<NoticeMessage>(responses.FirstOrDefault());
+        }
+
+        [Fact]
+        public async Task Allow_DirectOnly_In_Channel()
+        {
+            var msg = new PrivateMessage("private", ".cb secret i suck eggs");
+            var responses = await orchestrator.RespondAsync(msg).ToListAsync();
+            Assert.NotEmpty(responses);
+            Assert.IsAssignableFrom<PrivateMessage>(responses.FirstOrDefault());
+            var resp = responses.FirstOrDefault() as PrivateMessage;
+            Assert.Equal("secret sealed", resp.Message);
+        }
+
         public class TestProcessor : CommandProcessor
         {
             [Command("hello {user}")]
             public PrivateMessage SayHello(string user) => new PrivateMessage("nobody", $"hello, {user}");
+
+            [Command("potato {first} {second}")]
+            public PrivateMessage CheckTwoStrings(string second, string first) => new PrivateMessage("nobody", $"got {first} and {second}");
+
+            [Command("ping")]
+            public PrivateMessage PingPong() => new PrivateMessage("nobody", "pong");
+
+            [Command("add {a} {b}")]
+            public PrivateMessage Add(int a, int b) => new PrivateMessage("nobody", (a + b).ToString());
+
+            [Command("schedule {meetingName} {time}")]
+            public PrivateMessage Schedule(string meetingName, DateTime time) => new PrivateMessage("nobody", $"{meetingName} at {time:d}");
+
+            [Command("secret {dirty}"), DirectOnly]
+            public PrivateMessage TakeSecret(string dirty) => new PrivateMessage("nobody", "secret sealed");
         }
     }
 }
