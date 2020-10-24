ff9dc8a1ae7324b37e678e65141d9bd3134a7b2d
halomademeapc <alex@halomademeapc.com>
2020-10-23 22:38:01 -0400

    register commands

--- src/GravyBot.Commands/Class1.cs
+++ src/GravyBot.Commands/Class1.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace GravyBot.Commands
 {
@@ -133,23 +134,61 @@ namespace GravyBot.Commands
 
         private void RegisterCommand(CommandAttribute command, MethodInfo method)
         {
+            if (bindings.ContainsKey(command.Command))
+            {
+                var existingCommand = bindings[command.Command];
+                throw new DuplicateCommandException(command.Command, existingCommand.Method, method);
+            }
 
+            bindings[command.Command] = new CommandBinding(command, method);
         }
 
-        private struct CommandBinding
+    }
+    public struct CommandBinding
+    {
+        public MethodInfo Method;
+        public bool IsChannelOnly;
+        public bool IsDirectOnly;
+        public TimeSpan? RateLimitPeriod;
+        public bool IsRateLimited => RateLimitPeriod.HasValue;
+        public bool IsAsync;
+        public bool ProducesMultipleResponses;
+        public CommandAttribute Command;
+
+        public CommandBinding(CommandAttribute command, MethodInfo method)
         {
-            public Type ProcessorType;
-            public MethodInfo CommandMethod;
-            public bool IsChannelOnly;
-            public bool IsDirectOnly;
-            public TimeSpan? RateLimitPeriod;
-            public bool IsRateLimited => RateLimitPeriod.HasValue;
-            public bool IsAsync;
+            if (!(typeof(Task<IClientMessage>).IsAssignableFrom(method.ReturnType) || typeof(IClientMessage).IsAssignableFrom(method.ReturnType) || typeof(IAsyncEnumerable<IClientMessage>).IsAssignableFrom(method.ReturnType) || typeof(IEnumerable<IClientMessage>).IsAssignableFrom(method.ReturnType)))
+                throw new ArgumentException($"Method {method.ReflectedType.Name}.{method.Name} must return an IClientMessage, an IEnumerable of IClientMessage, or their async counterparts.");
+
+            Method = method;
+            IsChannelOnly = method.HasAttribute<ChannelOnlyAttribute>();
+            IsDirectOnly = method.HasAttribute<DirectOnlyAttribute>();
+            RateLimitPeriod = method.GetAttribute<RateLimitAttribute>()?.CooldownPeriod;
+            IsAsync = typeof(Task<IClientMessage>).IsAssignableFrom(method.ReturnType) || typeof(IAsyncEnumerable<IClientMessage>).IsAssignableFrom(method.ReturnType);
+            ProducesMultipleResponses = typeof(IEnumerable<IClientMessage>).IsAssignableFrom(method.ReturnType) || typeof(IAsyncEnumerable<IClientMessage>).IsAssignableFrom(method.ReturnType);
+            Command = command;
+
+            if (IsChannelOnly && IsDirectOnly)
+                throw new ArgumentException($"A command cannot be limited to both only channels and only direct messages.");
         }
     }
     internal static class Extensions
     {
         public static IEnumerable<CommandAttribute> GetCommands(this MethodInfo method) => method.GetCustomAttributes<CommandAttribute>();
         public static bool IsCommand(this MethodInfo method) => method.GetCommands().Any();
+        public static bool HasAttribute<TAttribute>(this MethodInfo method) where TAttribute : Attribute => method.GetCustomAttributes<TAttribute>().Any();
+        public static TAttribute GetAttribute<TAttribute>(this MethodInfo method) where TAttribute : Attribute => method.GetCustomAttributes<TAttribute>().FirstOrDefault();
+    }
+    public class DuplicateCommandException : Exception
+    {
+        public DuplicateCommandException(string commandName, MethodInfo attempted, MethodInfo conflicting)
+            : base($"Could not register {attempted.ReflectedType.Name}.{attempted.Name} to {commandName} because it is already registered to {conflicting.ReflectedType.Name}.{conflicting.Name}")
+        {
+            AttemptedCommand = attempted;
+            ConflictingCommand = conflicting;
+        }
+
+        public MethodInfo AttemptedCommand { get; private set; }
+        public MethodInfo ConflictingCommand { get; private set; }
     }
 }
